afd60f3f9f282ce025c0eefdb381382142f7b8ab
Ata Tangban <atatangban@live.com>
2017-05-02 11:52:16 +0100

    setup

---
+++ www/includes/db.php
@@ -0,0 +1,28 @@
+<?php 
+ # test.php sandbox
+
+define('DBNAME', 'iBlog');
+define('DBUSER', 'root');
+define('DBPASS', 'THINKandflyy');
+
+
+
+
+try {
+	#prepare a PDO instance
+	$conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
+
+	#set verbose error modes
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+} catch(PDOException $e) {
+	echo $e->getMessage();
+}
+
+
+
+
+
+
+
+?>
